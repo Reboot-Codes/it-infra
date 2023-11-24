f6bdc752586a01b41d64aaf47b450989653f6676
Reboot-Codes <git@reboot-codes.com>
2023-11-24 15:20:17 -0700

    Add `git` to service list.

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Hej! Order here is important!
-services=("website" "reverseproxy")
+services=("website" "git" "reverseproxy")
 
 for service in "${!services[@]}"; do
   ansible-playbook ./containers/$service/up.playbook.yml --vars "@vars.yml"
