9e4ba263e9acc164c7be9ff1f53ad6196dd5c2e4
Vincent Guttmann <vincent.guttmann@gmx.de>
2023-11-27 18:04:24 +0100

    Added the client setup file

---
+++ client-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apk add openssh shadow python3 sshpass bash
+rc-update add sshd
+service sshd start
+
+sed -i s/ash/bash/ /etc/passwd
+sed -i s/#PermitRootLogin\ prohibit-password/PermitRootLogin\ yes/ /etc/ssh/sshd_config
+sed -i s/#PubkeyAuthentication/PubkeyAuthentication/ /etc/ssh/sshd_config
+reboot
